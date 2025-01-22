3d4871990189cc5d0acbafb65aa07ff4d813ba7c
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-01-22 15:23:04 -0800

    Update app.js

--- kelly-virtual-assistant/app.js
+++ kelly-virtual-assistant/app.js
@@ -6,6 +6,7 @@ const content = document.querySelector('.content')
 function speak(text){
     const text_speak = new SpeechSynthesisUtterance(text);
 
+	// Set properties for the speech (rate, volume, pitch)
     text_speak.rate = 1;
     text_speak.volume = 1;
     text_speak.pitch = 1;
